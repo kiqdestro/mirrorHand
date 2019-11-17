100a966963b22dcbd612958ab47e25f1caef0c3b
Caíque Destro <caiquedestro@gmail.com>
2019-11-17 19:33:34 -0300

    Control directly from Raspberry

--- gloveRead/main.py
+++ gloveRead/main.py
@@ -2,12 +2,14 @@ import time
 import serialinput as si
 import numpy as np
 import matplotlib.pyplot as plt
+from adafruit_servokit import ServoKit
 
 serial = si.InitSerial()
 maximum = [0]*10
 minimum = [0]*10
 values = [0]*10
 value = 0
+ctrl = ServoControl()
 
 def main():
 
@@ -34,16 +36,38 @@ def main():
 
     while(True):
         value = si.ReadSerial(serial)                                   #Loop
-        #print("Sensor", i, Values[i], sep=' - ')                        #Print sensor values
-        normalized_value = (1-((value[1]- minimum[value[0]])/(maximum[value[0]]-minimum[value[0]]))) #Convert Input to a rotation value between 0 and 90 deg
+        #print("Sensor", i, Values[i], sep=' - ')                       #Print sensor values
+        normalized_value = (1-((value[1]- minimum[value[0]])/(maximum[value[0]]-minimum[value[0]]))) # Normalizing
         if (normalized_value > 1):
             normalized_value = 1
         if (normalized_value < 0):
             normalized_value = 0
-        print(value[1], minimum[value[0]], maximum[value[0]], minimum[value[0]], sep=' - ')                               
-        command_string = "setAttr(\"joint" + str(value[0]+1) + ".rotateZ\"," + str(normalized_value*180) + ");"     #Build MEL command_string
-        print(command_string)
-        # time.sleep(.1)                                                     #Delay
+
+        ctrl.setPos(value[0], normalized_value)
+
+class ServoControl():
+	def __init__(self):
+        
+        # EXAMPLES:
+        # kit.servo[0].angle = 180
+        # kit.servo[0].actuation_range = 160
+        # kit.servo[0].set_pulse_width_range(1000, 2000)
+
+		kit = ServoKit(channels = 16)
+		kit.servo[0].set_pulse_width_range(625, 2580)
+		kit.servo[1].set_pulse_width_range(500, 2620)
+		kit.servo[2].set_pulse_width_range(500, 2620)
+		kit.servo[3].set_pulse_width_range(540, 2600)
+		kit.servo[4].set_pulse_width_range(540, 2650)
+		kit.servo[5].set_pulse_width_range(540, 2650) #A
+		kit.servo[6].set_pulse_width_range(520, 2600) #B
+		kit.servo[7].set_pulse_width_range(520, 2600) #C *this one is turning only something around 120 degrees
+		kit.servo[8].set_pulse_width_range(520, 2600) #D
+		kit.servo[9].set_pulse_width_range(520, 2650) #E
+		kit.servo[10].set_pulse_width_range(500, 2650) #F
+
+	def setPos(self, servoID, position):
+		kit.servo[servoID].angle = (position * 180)
 
 if __name__ == "__main__":
     main()
