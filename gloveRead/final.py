4eef4f455b0b430882cf8ecbb37f173e5568238c
Caíque Destro <caiquedestro@gmail.com>
2019-11-24 02:29:18 -0300

    Testing bluetooth

---
+++ gloveRead/final.py
@@ -0,0 +1,17 @@
+from adafruit_servokit import ServoKit
+import RPi.GPIO as GPIO
+import serial
+import time
+
+bluetoothSerial = serial.Serial( "/dev/rfcomm0", baudrate=9600 )
+ 
+count = None
+while count == None:
+    try:
+        count = int(raw_input( "Please enter the number of times to blink the L$
+    except:
+        pass    # Ignore any errors that may occur and try again
+ 
+ 
+bluetoothSerial.write( str(count) )
+print bluetoothSerial.readline()
