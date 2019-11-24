737fb1c8a9022f07506d3bc8a541ee71600d18ff
Caíque Destro <caiquedestro@gmail.com>
2019-11-24 02:35:35 -0300

    input

--- gloveRead/final.py
+++ gloveRead/final.py
@@ -8,7 +8,7 @@ bluetoothSerial = serial.Serial( "/dev/rfcomm0", baudrate=9600 )
 count = None
 while count == None:
     try:
-        count = int(raw_input( "Please enter the number of times to blink the LED"))
+        count = input( "Please enter the number of times to blink the LED: ")
     except:
         pass    # Ignore any errors that may occur and try again
  
