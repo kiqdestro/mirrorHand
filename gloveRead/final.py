3c79c2bbf780b56a4f8814993495b723f1edfc8a
Caíque Destro <caiquedestro@gmail.com>
2019-11-24 02:46:57 -0300

    rteg

--- gloveRead/final.py
+++ gloveRead/final.py
@@ -7,12 +7,7 @@ import time
 
 bluetoothSerial = serial.Serial( "/dev/rfcomm0", baudrate=9600 )
  
-count = None
-while count == None:
-    try:
-        count = input( "Please enter the number of times to blink the LED: ")
-    except:
-        pass    # Ignore any errors that may occur and try again
+count = 5
  
  
 bluetoothSerial.write( str(count) )
