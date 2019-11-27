b4cec0528ca9b33e344c9f044ed2ddf9be5292fd
Caíque Destro <caiquedestro@gmail.com>
2019-11-27 02:11:56 -0300

    qerwtyj

--- gloveRead/final.py
+++ gloveRead/final.py
@@ -149,17 +149,17 @@ def main():
                     # ordem: polegar, indicador, medio, anelar, minimo (desativado , [0, 1], [2, 3], [4, 5], [6, 7])
 
                     if(i == 0 or i == 1):
-                        dedos[1] += (normalized_value / 4.0)
+                        dedos[1] += (normalized_value / 2.0)
                         # testeKIQ += "i: {}, valor: {} | ".format(i, (old_normalized_value / 4.0))
                     
                     elif(i == 2 or i == 3):
-                        dedos[2] += (normalized_value / 4.0)
+                        dedos[2] += (normalized_value / 2.0)
 
                     elif(i == 4 or i == 5):
-                        dedos[3] += (normalized_value / 4.0)
+                        dedos[3] += (normalized_value / 2.0)
 
                     elif(i == 6 or i == 7):
-                        dedos[4] += (normalized_value / 4.0) 
+                        dedos[4] += (normalized_value / 2.0) 
 
                     
 
